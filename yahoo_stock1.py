d2216221887f79493564a4d01ffbd428aa39ef0d
xiaozhiqi2016 <329275108@qq.com>
2016-07-22 10:31:31 +0800

    再此基础上加入判断页面的状态码,如果页面不存在则不抓取内容存入本地文件

--- yahoo_stock1.py
+++ yahoo_stock1.py
@@ -15,11 +15,13 @@ def download_stock_data_in_period(stock_list, start , end):
         url = url + qs
         fname = '%s_%d%d%d_%d%d%d.csv' % (sid,start.year,start.month,start.day,
                                             end.year, end.month, end.day)
-        print('downloading %s from %s' % (fname, url))
-        urllib.urlretrieve(url, fname)
+        if int(urllib.urlopen(url).getcode()) != 404:
+            print('downloading %s from %s' % (fname, url))
+            urllib.urlretrieve(url, fname)
+
 
 if __name__ == '__main__':
-    stock_list = ['300001.sz', '300002.sz']
+    stock_list = ['300001.sz','300002.sz', '304002.sz']
     start = datetime.date(year=2016, month=6, day=17)
     end = datetime.date(year=2016, month=7, day=17)
     download_stock_data_in_period(stock_list,start,end)
