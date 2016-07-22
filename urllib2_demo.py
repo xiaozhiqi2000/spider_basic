2be85bee9a41b20d809c0563ea23a8237d3ee0a3
xiaozhiqi2016 <329275108@qq.com>
2016-07-22 14:47:40 +0800

    nothing

--- urllib2_demo.py
+++ urllib2_demo.py
@@ -12,13 +12,14 @@
 import urllib2
 
 def urlopen():
-    url = 'http://blog.kamidox.com/no-exists'
+    url = 'http://blog.kamidox.com'
     try:
         s = urllib2.urlopen(url, timeout=3)
     except urllib2.HTTPError, e:
         print(e)
     else:
         print(s.read(100))
+        s.close()
 
 if __name__ == '__main__':
     urlopen()
